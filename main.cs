65389e0ffeec8c7e69dcc4931bce0c64fe7d0778
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-23 17:22:16 +0000

    added code

--- main.cs
+++ main.cs
@@ -5,24 +5,23 @@
 
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
+class Program {
+  public static void Main(string[] args)
     {
-        // This function accepts user input
-        string streetNumber;
-        int streetName;
+        // This function states user's address input.
+        string streetName;
+        int streetNumber;
 
-        Console.WriteLine("This program gets a user's street number and street name.");
+        Console.WriteLine("This program will retell you your street name and street number.");
         Console.WriteLine("");
 
-        Console.Write("Enter your street number: ");
-        streetNumber = Console.ReadLine();
         Console.Write("Enter your street name: ");
-        streetName = Convert.ToInt32(Console.ReadLine());
+        streetName = Console.ReadLine();
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("");
-        Console.WriteLine("Your address is: " + streetNumber +" " + streetName + ".");
+        Console.WriteLine("Your address is: " + streetName + " " + streetNumber + ".");
 
         Console.WriteLine("\nDone.");
     }
